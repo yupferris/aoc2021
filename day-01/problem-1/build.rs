4a7368302de0e928cc0750afe9ea418802281c62
ferris <yupferris@gmail.com>
2021-12-08 00:23:24 +0100

    day 1 problem 1 solution
    As expected, doing AoC like this was not very fun, doubt I'll go much farther than this.. ok it was kinda fun :D

---
+++ day-01/problem-1/build.rs
@@ -0,0 +1,67 @@
+use kaze::*;
+
+use std::env;
+use std::fs::File;
+use std::io::Result;
+use std::path::Path;
+
+fn main() -> Result<()> {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("modules.rs");
+    let file = File::create(&dest_path).unwrap();
+
+    let c = Context::new();
+
+    sim::generate(Sweeper::new("sweeper", 32, &c).m, sim::GenerationOptions::default(), file)
+}
+
+#[allow(dead_code)]
+struct Sweeper<'a> {
+    pub m: &'a Module<'a>,
+
+    pub depth: &'a Input<'a>,
+
+    pub larger_measurements: &'a Output<'a>,
+    pub larger_measurements_valid: &'a Output<'a>,
+}
+
+impl<'a> Sweeper<'a> {
+    pub fn new(instance_name: impl Into<String>, depth_bit_width: u32, p: &'a impl ModuleParent<'a>) -> Sweeper<'a> {
+        let m = p.module(instance_name, "Sweeper");
+
+        let depth = m.input("depth", depth_bit_width);
+        let depth_valid = m.input("depth_valid", 1);
+
+        let state_valid = m.reg("state_valid", 1);
+        state_valid.default_value(false);
+        state_valid.drive_next(state_valid | depth_valid);
+
+        let prev_depth = m.reg("prev_depth", depth_bit_width);
+        prev_depth.default_value(0u32);
+        prev_depth.drive_next(if_(depth_valid, {
+            depth
+        }).else_({
+            prev_depth
+        }));
+
+        let larger_measurement = state_valid & depth_valid & depth.gt(prev_depth);
+
+        let larger_measurements_bit_width = 32;
+        let larger_measurements_reg = m.reg("larger_measurements_reg", larger_measurements_bit_width);
+        larger_measurements_reg.default_value(0u32);
+        larger_measurements_reg.drive_next(if_(larger_measurement, {
+            larger_measurements_reg + m.lit(1u32, larger_measurements_bit_width)
+        }).else_({
+            larger_measurements_reg
+        }));
+
+        Sweeper {
+            m,
+
+            depth,
+
+            larger_measurements: m.output("larger_measurements", larger_measurements_reg),
+            larger_measurements_valid: m.output("larger_measurements_valid", state_valid),
+        }
+    }
+}
--- day-01/problem-1/src/lib.rs
+++ day-01/problem-1/src/lib.rs
@@ -1,8 +1,29 @@
 #[cfg(test)]
 mod tests {
+    mod modules {
+        include!(concat!(env!("OUT_DIR"), "/modules.rs"));
+    }
+
+    use modules::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn problem_1() {
+        let input = include_str!("input.txt");
+
+        let mut m = Sweeper::new();
+
+        m.reset();
+
+        for line in input.lines() {
+            let depth = str::parse::<u32>(line).expect("Couldn't parse depth");
+            m.depth = depth;
+            m.depth_valid = true;
+            m.prop();
+            m.posedge_clk();
+        }
+
+        m.prop();
+        assert!(m.larger_measurements_valid);
+        assert_eq!(1532, m.larger_measurements);
     }
 }
