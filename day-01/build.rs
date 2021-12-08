b1a7d02abca2d946e5515fdcc752dceba062e530
ferris <yupferris@gmail.com>
2021-12-08 01:13:41 +0100

    day 1 part 2 solution
    Definitely made it harder than it had to be by pipelining inputs in the RTL instead of unpacking in the driver, but that also made it more fun/worthwhile imo, so glad I did it that way.
    
    That said, I had one too few valid stages, which meant the answer was off by one, and this was particularly hard to track down!
    
    One last thing: there's far more type annotations that I'd like; that's another way in which kaze really sucks rn.

--- day-01/build.rs
+++ day-01/build.rs
@@ -8,11 +8,14 @@ use std::path::Path;
 fn main() -> Result<()> {
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("modules.rs");
-    let file = File::create(&dest_path).unwrap();
+    let mut file = File::create(&dest_path).unwrap();
 
     let c = Context::new();
 
-    sim::generate(Sweeper::new("sweeper", 32, &c).m, sim::GenerationOptions::default(), file)
+    sim::generate(Sweeper::new("sweeper", 32, &c).m, sim::GenerationOptions::default(), &mut file)?;
+    sim::generate(Slider::new("slider", 32, &c).m, sim::GenerationOptions::default(), &mut file)?;
+
+    Ok(())
 }
 
 #[allow(dead_code)]
@@ -56,6 +59,93 @@ impl<'a> Sweeper<'a> {
         }));
 
         Sweeper {
+            m,
+
+            depth,
+            depth_valid,
+
+            larger_measurements: m.output("larger_measurements", larger_measurements_reg),
+            larger_measurements_valid: m.output("larger_measurements_valid", state_valid),
+        }
+    }
+}
+
+#[allow(dead_code)]
+struct Slider<'a> {
+    pub m: &'a Module<'a>,
+
+    pub depth: &'a Input<'a>,
+    pub depth_valid: &'a Input<'a>,
+
+    pub larger_measurements: &'a Output<'a>,
+    pub larger_measurements_valid: &'a Output<'a>,
+}
+
+impl<'a> Slider<'a> {
+    pub fn new(instance_name: impl Into<String>, depth_bit_width: u32, p: &'a impl ModuleParent<'a>) -> Slider<'a> {
+        let m = p.module(instance_name, "Slider");
+
+        let depth = m.input("depth", depth_bit_width);
+        let depth_valid = m.input("depth_valid", 1);
+
+        let mut prev_depths: Vec<&dyn Signal<'_>> = Vec::new();
+        for i in 0..2 {
+            let prev_depth = m.reg(format!("prev_depth_{}", i), depth_bit_width);
+            prev_depth.drive_next(if_(depth_valid, {
+                if i == 0 {
+                    depth
+                } else {
+                    prev_depths[i - 1]
+                }
+            }).else_({
+                prev_depth
+            }));
+            prev_depths.push(prev_depth);
+        }
+
+        let mut state_valids = Vec::new();
+        for i in 0..3 {
+            let state_valid = m.reg(format!("state_valid_{}", i), 1);
+            state_valid.default_value(false);
+            state_valid.drive_next(if_(depth_valid, {
+                if i == 0 {
+                    m.lit(true, 1)
+                } else {
+                    state_valids[i - 1]
+                }
+            }).else_({
+                state_valid
+            }));
+            state_valids.push(state_valid);
+        }
+
+        let state_valid = *state_valids.last().unwrap();
+
+        let mut sum: &dyn Signal<'_> = depth;
+        for &prev_depth in &prev_depths {
+            sum = sum + prev_depth;
+        }
+
+        let prev_sum = m.reg("prev_sum", depth_bit_width);
+        prev_sum.default_value(0u32);
+        prev_sum.drive_next(if_(depth_valid, {
+            sum
+        }).else_({
+            prev_sum
+        }));
+
+        let larger_measurement = state_valid & depth_valid & sum.gt(prev_sum);
+
+        let larger_measurements_bit_width = 32;
+        let larger_measurements_reg = m.reg("larger_measurements_reg", larger_measurements_bit_width);
+        larger_measurements_reg.default_value(0u32);
+        larger_measurements_reg.drive_next(if_(larger_measurement, {
+            larger_measurements_reg + m.lit(1u32, larger_measurements_bit_width)
+        }).else_({
+            larger_measurements_reg
+        }));
+
+        Slider {
             m,
 
             depth,
--- day-01/src/lib.rs
+++ day-01/src/lib.rs
@@ -26,4 +26,23 @@ mod tests {
         assert!(m.larger_measurements_valid);
         assert_eq!(1532, m.larger_measurements);
     }
+
+    #[test]
+    fn part_2() {
+        let mut m = Slider::new();
+
+        m.reset();
+
+        for line in INPUT.lines() {
+            let depth = str::parse::<u32>(line).expect("Couldn't parse depth");
+            m.depth = depth;
+            m.depth_valid = true;
+            m.prop();
+            m.posedge_clk();
+        }
+
+        m.prop();
+        assert!(m.larger_measurements_valid);
+        assert_eq!(1571, m.larger_measurements);
+    }
 }
